2c5cbbb0b6af24638eb58019804c21c9cd3614ac
Diegosilcor <diegosilvacordoba@hotmail.com>
2022-10-21 10:55:47 +0200

    Http y expres

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hello World');
+// Con Node.js
+// const http = require('http')
+// const fs = require('fs')
+
+// const server = http.createServer((req, res) => {
+//   const read = fs.createReadStream('./static/index.html')
+//   read.pipe(res);
+// });
+
+// server.listen(3000)
+// console.log(`Server on port ${3000}`);
+
+
+// Con Express
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+  res.sendFile('./static/index.html', {
+    root: __dirname
+  })
+})
+
+app.listen(3000)
+console.log(`Server on port ${3000}`);
+
